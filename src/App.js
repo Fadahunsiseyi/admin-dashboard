fb244a830ee37994f4f43512c1f43eb0dfae6223
fadahunsiseyi <oluwaseyisamuel018@gmail.com>
2021-09-11 22:49:21 +0100

    I added the Admin dashboard info widgets

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import Sidebar from "./components/sidebar/Sidebar";
 import TopBar from "./components/topbar/Topbar";
 import './app.css'
+import Home from "./components/pages/home/Home";
 
 
 function App() {
@@ -9,7 +10,7 @@ function App() {
      <TopBar/>
      <div className="container">
        <Sidebar/>
-       <div className="others">Other Pages</div>
+       <Home/>
      </div>
     </div>
   );
---
+++ src/components/pages/home/Home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import FeaturedInfo from '../../featuredInfo/FeaturedInfo'
+import './home.scss'
+
+export default function Home() {
+    return (
+        <div className="home">
+            <FeaturedInfo/>
+        </div>
+    )
+}
